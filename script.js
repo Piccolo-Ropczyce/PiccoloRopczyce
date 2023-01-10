236a313a4f46e5f39ec38570d5f323a3b898ac63
kimi1723 <100174940+kimi1723@users.noreply.github.com>
2023-01-10 23:45:37 +0100

    Dodanie js, zmiana ikon, kilka komentarzy w html/css

--- script.js
+++ script.js
@@ -1,15 +1,78 @@
-class klasa {
-	constructor(name) {
-		this.name = name
+// Klasa do stworzenia pizzy
+class PizzaCreator {
+	constructor(ingredients, smallPizzaPrice, bigPizzaPrice, pizzaType) {
+		this.ingredients = ingredients,
+		this.smallPizzaPrice = smallPizzaPrice,
+		this.bigPizzaPrice = bigPizzaPrice,
+		this.pizzaType = pizzaType
 	}
+};
+
+// Funkcja powyzszej klasy tworzaca li na stronie z stworzona pizza
+PizzaCreator.prototype.addPizzaToUl = function () {
+	// Pobieranie elementow z HTML
+	const pizzaUlContainer = document.querySelector('.pizza-list-box');
+
+	// Tworzenie elementow HTML
+	const createLi = document.createElement('li');
+	const createBtnSmallPizza = document.createElement('button');
+	const createBtnBigPizza = document.createElement('button');
+	const createDiv = document.createElement('div');
+
+	// Set up tekstu elementow dodawanych do strony
+	createLi.textContent = this.ingredients;
+	createBtnSmallPizza.textContent = this.smallPizzaPrice;
+	createBtnBigPizza.textContent = this.bigPizzaPrice;
+
+	// Set up klas elementow dodawanych do strony
+	createLi.classList.add('li-pizza');
+	createDiv.classList.add('add-to-cart-buttons');
+
+	// Przypisanie elementow do odpowiednich struktur
+	createDiv.append(createBtnSmallPizza);
+	createDiv.append(createBtnBigPizza);
+	pizzaUlContainer.append(createLi);
+	pizzaUlContainer.append(createDiv);
+	createLi.append(createDiv);
 }
 
-klasa.prototype.say = function () {
-	console.log(`mistrzunio ty kurwo ${this.name}`)
+// Tablica zawierajaca wszystkie pizze
+const arrayContainingPizzas = [
+	{
+		ingredients: 'Sos pomidorowy, mozzarella',
+		smallPizzaPrice: 24,
+		bigPizzaPrice: 33,
+		pizzaType: 'vege',
+	},
+	{
+		ingredients: 'Sos pomidorowy, mozzarella,pomidorki koktajlowe, oliwki, świeża bazylia',
+		smallPizzaPrice: 29,
+		bigPizzaPrice: 42,
+		pizzaType: 'kurczak',
+	},
+]
+
+// Pobor wszystkich elementow z typami pizzy
+const liMenu = document.querySelectorAll('.li-menu');
+
+
+// H2 z pizzami (tak jak w rozacie) zeby nie bylo samych skladnikow uzupelnic nazwami pizz, typu neapolitanska itp
+
+// Funkcja tworzaca pizze na podstawie jej typu z uzyciem klasy
+const getPizza = e => {
+	let pizzaType = e.target.dataset.name;
+
+	arrayContainingPizzas.forEach(pizza => {
+		for (const key in pizza) {
+			if (pizza[key] === pizzaType) {
+
+				const chosenPizza = new PizzaCreator(pizza.ingredients, pizza.smallPizzaPrice, pizza.bigPizzaPrice, pizza.pizzaType);
+
+				chosenPizza.addPizzaToUl();
+			}
+		}
+	})
 }
 
-const mistrz = new klasa('Konrad')
 
-mistrz.say()
-
-//
+liMenu.forEach(el => el.addEventListener('click', getPizza));
